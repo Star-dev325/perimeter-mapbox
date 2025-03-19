e927f9c6486d85f83608fdbf22ac298d578d2035
Star-dev325 <bounnarath.l121@gmail.com>
2025-03-19 11:22:21 -0400

    add s3 pulumi module

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
-import * as fs from "fs";
-import mime from "mime";
+const s3folder = require("./s3folder.js");
 
-// Create an S3 bucket with website hosting
-const bucket = new aws.s3.Bucket("myFrontendBucket", {
-    website: {
-        indexDocument: "index.html",
-        errorDocument: "index.html",
-    },
-});
+// Create an instance of the S3Folder component
+let folder = new s3folder.S3Folder("pulumi-static-site", "./build");
 
-// Get all files from the "dist" folder and upload them
-const siteDir = "./build";
-fs.readdirSync(siteDir).forEach(file => {
-    new aws.s3.BucketObject(file, {
-        bucket: bucket,
-        source: new pulumi.asset.FileAsset(`${siteDir}/${file}`),
-        contentType: mime.getType(file) || "application/octet-stream"
-    });
-});
-
-// Apply a public bucket policy
-new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: bucket.id,
-    policy: bucket.id.apply(id => JSON.stringify({
-        Version: "2012-10-17",
-        Statement: [{
-            Effect: "Allow",
-            Principal: "*",
-            Action: "s3:GetObject",
-            Resource: `arn:aws:s3:::${id}/*`
-        }]
-    })),
-});
-
-// Export the bucket website URL
-export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+// Export `folder` output properties as stack outputs
+exports.bucketName = folder.bucketName;
+exports.websiteUrl = folder.websiteUrl;
