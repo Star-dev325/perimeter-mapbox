778764e14a3a5365986c3378a3d4b10f710b4258
Star-dev325 <bounnarath.l121@gmail.com>
2025-03-19 15:19:02 -0400

    add bucket policy

--- index.js
+++ index.js
@@ -41,22 +41,22 @@ files.forEach((file) => {
 });
 
 // Apply a public bucket policy
-// new aws.s3.BucketPolicy("bucketPolicy", {
-//     bucket: bucket.id,
-//     policy: bucket.id.apply((id) =>
-//         JSON.stringify({
-//             Version: "2012-10-17",
-//             Statement: [
-//                 {
-//                     Effect: "Allow",
-//                     Principal: "*",
-//                     Action: "s3:GetObject",
-//                     Resource: `arn:aws:s3:::${id}/*`,
-//                 },
-//             ],
-//         })
-//     ),
-// });
+new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: bucket.id,
+    policy: bucket.id.apply((id) =>
+        JSON.stringify({
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Effect: "Allow",
+                    Principal: "*",
+                    Action: "s3:GetObject",
+                    Resource: `arn:aws:s3:::${id}/*`,
+                },
+            ],
+        })
+    ),
+});
 
 // Export the bucket website URL
 export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
